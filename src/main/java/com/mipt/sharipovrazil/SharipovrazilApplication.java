834b509f7824612580ea5b08c7807a7a7b279280
Sharipov Razil <sharipov.rr@phystech.edu>
2026-06-25 06:51:18 +0300

    Приложение и TraceIdFilter и AccessLogFilter

--- src/main/java/com/mipt/sharipovrazil/SharipovrazilApplication.java
+++ src/main/java/com/mipt/sharipovrazil/SharipovrazilApplication.java
@@ -2,19 +2,9 @@ package com.mipt.sharipovrazil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SharipovrazilApplication {
-    /**
-     * Точка входа в приложение.
-     *
-     * <p>Запускает Spring Boot приложение, создавая и конфигурируя
-     * контекст Spring, встроенный веб-сервер и все бины.</p>
-     *
-     * @param args аргументы командной строки, переданные при запуске
-     */
     public static void main(String[] args) {
         SpringApplication.run(SharipovrazilApplication.class, args);
     }
