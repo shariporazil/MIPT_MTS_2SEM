b41d784837dbed670c77ae7a5e75548a715b29d5
Sharipov Razil <sharipov.rr@phystech.edu>
2026-06-25 06:47:48 +0300

    DTO для аутентификации

---
+++ src/main/java/com/mipt/sharipovrazil/dto/task/TaskRequest.java
@@ -0,0 +1,14 @@
+package com.mipt.sharipovrazil.dto.task;
+
+public class TaskRequest {
+    private String title;
+    private String description;
+    private Boolean completed;
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public Boolean getCompleted() { return completed; }
+    public void setCompleted(Boolean completed) { this.completed = completed; }
+}
